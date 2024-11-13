10ccf7caa76f0867183b26828d414d9a9a1e2266
“Wendi <wendi.huangg@gmail.comgit config user.namegit config user.emailgit config --global init.defaultBranch main git config --global pull.rebase falsegit config --global user.email wendi.huangg@gmail.com>
2024-11-13 09:46:04 -0800

    finished react routing, created placeholders components: Inventory, WareHouse, HomePage (for changing later)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Inventory from './components/Inventory/Inventory';
+import WareHouse from './components/Warehouse/Warehouse';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router> 
+      <Routes>
+          <Route path ='/' element = {<Home />}/>
+          <Route path ='/inventory' element = {<Inventory />}/>
+          <Route path ='/warehouse' element = {<WareHouse />}/>
+      </Routes>
+    </Router>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/WareHouse/WareHouse.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WareHouse = () => {
+    return <h1> WareHouse </h1>
+};
+
+export default WareHouse;
