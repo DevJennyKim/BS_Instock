a4fecd91e77a049eb09d749e4af32c4cab5f9174
Haeun[Jenny] Kim <dev.jennyK@gmail.com>
2024-11-16 11:18:47 -0700

    installed the sweetalert

--- src/pages/WarehouseFormPage/WarehouseFormPage.jsx
+++ src/pages/WarehouseFormPage/WarehouseFormPage.jsx
@@ -1,26 +1,27 @@
-import "./WarehouseFormPage.scss";
-import { useEffect, useState } from "react";
-import backIcon from "../../assets/Icons/arrow_back-24px.svg";
-import FormField from "../../components/FormField/FormField";
-import { useParams, useNavigate } from "react-router-dom";
-import * as api from "../../api/instock-api";
-import validator from "validator";
-import { phone } from "phone";
-import PropTypes from "prop-types";
+import './WarehouseFormPage.scss';
+import { useEffect, useState } from 'react';
+import backIcon from '../../assets/Icons/arrow_back-24px.svg';
+import FormField from '../../components/FormField/FormField';
+import { useParams, useNavigate } from 'react-router-dom';
+import * as api from '../../api/instock-api';
+import validator from 'validator';
+import { phone } from 'phone';
+import PropTypes from 'prop-types';
+import Swal from 'sweetalert2';
 
 function WarehouseForm({ action }) {
   const { id } = useParams();
   const navigate = useNavigate();
   const [errorMessages, setErrorMessages] = useState({});
   const [newWarehouse, setNewWarehouse] = useState({
-    warehouse_name: "",
-    address: "",
-    city: "",
-    country: "",
-    contact_name: "",
-    contact_position: "",
-    contact_phone: "",
-    contact_email: "",
+    warehouse_name: '',
+    address: '',
+    city: '',
+    country: '',
+    contact_name: '',
+    contact_position: '',
+    contact_phone: '',
+    contact_email: '',
   });
 
   useEffect(() => {
@@ -29,35 +30,35 @@ function WarehouseForm({ action }) {
       const { id, created_at, updated_at, ...formFields } = warehouseDetails;
       setNewWarehouse(formFields);
     };
-    if (action === "update") {
+    if (action === 'update') {
       loadWarehouseDetails(id);
     }
   }, [action, id]);
 
   const warehouseDetailsConfigs = [
     {
-      label: "Warehouse Name",
-      name: "warehouse_name",
-      placeholder: "Warehouse Name",
+      label: 'Warehouse Name',
+      name: 'warehouse_name',
+      placeholder: 'Warehouse Name',
     },
-    { label: "Street Address", name: "address", placeholder: "Street Address" },
-    { label: "City", name: "city", placeholder: "City" },
-    { label: "Country", name: "country", placeholder: "Country" },
+    { label: 'Street Address', name: 'address', placeholder: 'Street Address' },
+    { label: 'City', name: 'city', placeholder: 'City' },
+    { label: 'Country', name: 'country', placeholder: 'Country' },
   ];
 
   const contactDetailsConfigs = [
     {
-      label: "Contact Name",
-      name: "contact_name",
-      placeholder: "Contact Name",
+      label: 'Contact Name',
+      name: 'contact_name',
+      placeholder: 'Contact Name',
     },
-    { label: "Position", name: "contact_position", placeholder: "Position" },
+    { label: 'Position', name: 'contact_position', placeholder: 'Position' },
     {
-      label: "Phone Number",
-      name: "contact_phone",
-      placeholder: "Phone Number",
+      label: 'Phone Number',
+      name: 'contact_phone',
+      placeholder: 'Phone Number',
     },
-    { label: "Email", name: "contact_email", placeholder: "Email" },
+    { label: 'Email', name: 'contact_email', placeholder: 'Email' },
   ];
 
   const handleFieldChange = (event) => {
@@ -72,18 +73,18 @@ function WarehouseForm({ action }) {
     const errors = {};
 
     for (const [key, value] of Object.entries(newWarehouse)) {
-      if (key === "contact_email") {
+      if (key === 'contact_email') {
         if (!validator.isEmail(value)) {
-          errors[key] = "Please enter a valid email address";
+          errors[key] = 'Please enter a valid email address';
         }
-      } else if (key === "contact_phone") {
+      } else if (key === 'contact_phone') {
         if (!phone(value).isValid) {
           errors[key] =
-            "Please enter a valid phone number. Format: +X (XXX) XXX-XXXX";
+            'Please enter a valid phone number. Format: +X (XXX) XXX-XXXX';
         }
       }
       if (!value) {
-        errors[key] = "This field is required";
+        errors[key] = 'This field is required';
       }
     }
 
@@ -95,24 +96,25 @@ function WarehouseForm({ action }) {
     e.preventDefault();
 
     if (validateForm()) {
-      if (action === "add") {
+      if (action === 'add') {
         await api.addWarehouse(newWarehouse);
-      } else if (action === "update") {
+      } else if (action === 'update') {
         await api.updateWarehouse(newWarehouse, id);
+        navigate(-1);
       }
 
       setNewWarehouse({
-        warehouse_name: "",
-        address: "",
-        city: "",
-        country: "",
-        contact_name: "",
-        contact_position: "",
-        contact_phone: "",
-        contact_email: "",
+        warehouse_name: '',
+        address: '',
+        city: '',
+        country: '',
+        contact_name: '',
+        contact_position: '',
+        contact_phone: '',
+        contact_email: '',
       });
       setErrorMessages({});
-      console.log("Form Valid");
+      console.log('Form Valid');
     }
   };
 
@@ -128,7 +130,7 @@ function WarehouseForm({ action }) {
           />
         </div>
         <h1 className="warehouse-page__title">
-          {action === "add" ? "Add New Warehouse" : "Edit Warehouse"}
+          {action === 'add' ? 'Add New Warehouse' : 'Edit Warehouse'}
         </h1>
       </div>
 
@@ -168,7 +170,7 @@ function WarehouseForm({ action }) {
             Cancel
           </button>
           <button className="warehouse-form__button">
-            {action === "add" ? "+ Add Warehouse" : "Save"}
+            {action === 'add' ? '+ Add Warehouse' : 'Save'}
           </button>
         </div>
       </form>
